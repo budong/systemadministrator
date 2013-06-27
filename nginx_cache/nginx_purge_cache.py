bceee4959730d3812743024e3739e7217b673186
budong <sias_peiqiang@qq.com>
2013-06-27 08:54:33 +0800

    nginx_purge_cache

--- nginx_cache/nginx_purge_cache.py
+++ nginx_cache/nginx_purge_cache.py
@@ -7,7 +7,8 @@
 '''
 说明：
     1.本脚本用于清除nginx缓存文件
-    2.根据我的测试nginx缓存文件时，根据uri进行hash，因此可以模拟nginx对一个uri进行再
+    2.查看你的nginx是根据什么作为key来hash的，我的设置是 proxy_cache_key $uri$is_args$args;
+    因此nginx会根据$uri$is_args$args作为key进行hash，因此可以模拟nginx对一个key进行再
     hash找到相应的文件路径，删除（具体可随意找个缓存文件 more 一下看看)
     3.缓存设置 proxy_cache_path /data/mumayi/cache levels=1:2 keys_zone=cache_one:6000m inactive=15d max_size=200g;
     根据相应的配置，请做相应修改测试
