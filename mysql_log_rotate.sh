9fb50ef16b8527c3d54d556d165448440afad5b7
budong <sias_peiqiang@qq.com>
2013-07-27 00:17:27 +0800

    mysql

--- mysql_log_rotate.sh
+++ mysql_log_rotate.sh
@@ -1,18 +1,35 @@
-#!/bin/sh
+具体参见源码包：
+support-files/mysql-log-rotate.sh
 
-#crontab
-#02 02 * * * sh /root/sbin/mysql_log_rotate.sh
-
-DIR_BASE="/data/mysql_db"
-DIR_LOG="${DIR_BASE}/rotate"
-DIR_DATE=$(date +%Y/%m/%d)
-LAST_MONTH=`date -d last-month +'%Y/%m/%d'`
-
-#clear error_log and slow_log every month
-/bin/mkdir -p ${DIR_LOG}/${DIR_DATE} > /dev/null 2>&1
-/bin/mv ${DIR_BASE}/error.log ${DIR_LOG}/${DIR_DATE}/
-/bin/touch  ${DIR_BASE}/error.log
-/bin/mv ${DIR_BASE}/slow.log ${DIR_LOG}/${DIR_DATE}/
-/bin/touch  ${DIR_BASE}/slow.log
-
-rm -rf ${DIR_LOG}/${LAST_MONTH}/
+/data/mysql_db/slow.log {
+        # create 600 mysql mysql
+        notifempty
+    daily
+        rotate 3
+        missingok
+        compress
+    postrotate
+    # just if mysqld is really running
+    if test -x /usr/local/mysql/bin/mysqladmin && \
+       /usr/local/mysql/bin/mysqladmin -uroot -pbudong ping &>/dev/null
+    then
+       /usr/local/mysql/bin/mysqladmin -uroot -pbudong -e "flush logs"
+    fi
+    endscript
+}
+/data/mysql_db/error.log {
+        # create 600 mysql mysql
+        notifempty
+    daily
+        rotate 3
+        missingok
+        compress
+    postrotate
+    # just if mysqld is really running
+    if test -x /usr/local/mysql/bin/mysqladmin && \
+       /usr/local/mysql/bin/mysqladmin -uroot -pbudong ping &>/dev/null
+    then
+       /usr/local/mysql/bin/mysqladmin -uroot -pbudong -e "flush logs"
+    fi
+    endscript
+}
