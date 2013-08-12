7d5ae8276e4c2107d84739d5f041e2f4166048b9
budong <sias_peiqiang@qq.com>
2013-08-12 10:00:53 +0800

    nginx

--- nginx_site/install.sh
+++ nginx_site/install.sh
@@ -4,7 +4,7 @@ yum -y install gcc gcc-c++ autoconf automake
 2.安装依赖库
 yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel
 3.安装nginx
-useradd -s /sbin/nologin wwww
+useradd -s /sbin/nologin www
 tar zxvf nginx-1.2.6.tar.gz
 cd nginx-1.2.6/
 ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module
