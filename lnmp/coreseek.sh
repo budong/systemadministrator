f6918830a87353e9874ca2e77437dede7aecf954
budong <sias_peiqiang@qq.com>
2013-09-05 10:15:21 +0800

    coreseek

--- lnmp/coreseek.sh
+++ lnmp/coreseek.sh
@@ -4,7 +4,7 @@ cd coreseek-3.2.13/mmseg-3.2.13
 ./bootstrap
 ./configure --prefix=/usr/local/mmseg
 make && make install
-cd coreseek-3.2.13
+cd ../
 cd csft-3.2.13/
 export LIBS="-liconv"
 ./configure --prefix=/usr/local/coreseek --with-mysql --with-mmseg --with-mmseg-includes=/usr/local/mmseg/include/mmseg --with-mmseg-libs=/usr/local/mmseg/lib
