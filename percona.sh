be02638964432ba52d2f64a82782ccd7ef945ae2
peiqiang <sias_peiqiang@163.com>
2013-10-24 23:50:49 +0800

    percona

---
+++ percona.sh
@@ -0,0 +1,6 @@
+#http://www.percona.com/software
+
+#instoll percona-toolkit
+wget http://www.percona.com/redir/downloads/percona-toolkit/LATEST/RPM/percona-toolkit-2.2.5-1.noarch.rpm
+rpm -ivh percona-toolkit-2.2.5-1.noarch.rpm
+
