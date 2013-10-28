559b952f075057f46630fc88e414ba83c8deb2cc
peiqiang <sias_peiqiang@163.com>
2013-10-28 11:25:01 +0800

    shell

--- shell_example.sh
+++ shell_example.sh
@@ -20,3 +20,19 @@ ls -S|head -1
 
 统计nginx下载软件的前十
 cat apk.a.log | awk '{print $7}' | sort | uniq -c | sort -rn | head -10
+
+删除apk软件
+vim rm_apk.sh
+#!/bin/sh
+
+#file=$1
+while read -r line
+do
+    #echo $line
+    rm -rf $line
+done < /root/sbin/soft_bak
+#done < $file
+
+vim soft_bak
+/data/mumayi/soft/2013/03/14/29/292928/linghuntanceqi_V1.3.0_mumayi_8c2f0.apk
+/data/mumayi/soft/2013/03/08/29/290117/zhongguotushuguan_V3.1_mumayi_80a5a.apk
